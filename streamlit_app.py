d8f77e94fd29204514a2954af08a784330f2af49
andrew <56230074+KrYthReX@users.noreply.github.com>
2025-10-27 01:34:45 +0000

    Created inital template to upload files onto app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
-import streamlit as st
+# streamlit run streamlit_app.py
 
-st.title("🎈 My new app")
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import requests
+
+st.title("🎈 Berry Analysis")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    # "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("This is currently a work in progress app, built to analyze and classify images uploaded by the user.")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+if uploaded_file:
+    image = Image.open(uploaded_file)
+# else:
+#     image = Image.open(requests.get("https://picsum.photos/200/120", stream=True).raw)
+
+edges = cv2.Canny(np.array(image), 100, 200)
+tab1, tab2 = st.tabs(["Detected edges", "Original"])
+tab1.image(edges, use_column_width=True)
+tab2.image(image, use_column_width=True)
