95136fa8ce6f5c3e8b42ce9815720b0c48ed4e83
andrew <56230074+KrYthReX@users.noreply.github.com>
2025-11-17 20:39:43 +0000

    Updated self.model to remove deprecated function. Updated to .DEFAULT as suggestion from warning.

--- models_J.py
+++ models_J.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.models import ResNet50_Weights
 
 # Dynamic flatten CNN
 class SimpleCNN(nn.Module):
@@ -41,7 +42,8 @@ class ResNet18(nn.Module):
 class ResNet50(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.model = models.resnet50(pretrained=True)
+        # self.model = models.resnet50(pretrained=True)
+        self.model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
